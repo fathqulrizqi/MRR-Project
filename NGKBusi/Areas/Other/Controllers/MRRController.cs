feda86f3d8b3bd80e4bad3f2fd29fb329848d418
faaay.adistiii <faaay.adistiii@gmail.com>
2025-03-26 11:40:32 +0700

    test

---
+++ NGKBusi/Areas/Other/Controllers/MRRController.cs
@@ -0,0 +1,62 @@
+﻿using NGKBusi.Models;
+using NGKBusi.Areas.Other.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+
+
+namespace NGKBusi.Areas.Other.Controllers
+{
+    public class MRRController : Controller
+    {
+
+        DefaultConnection db = new DefaultConnection();
+        MRRConnection dbm = new MRRConnection();
+
+        // GET: Other/MRR
+        public ActionResult Index()
+        {
+            var currUserId = User.Identity.GetUserId();
+            ViewBag.allowedUpdate = false;
+            ViewBag.navHide = true;
+            //if (Request.IsAuthenticated)
+            //{
+            //    ViewBag.allowedUpdate = db.Users_Menus_Roles.Where(x => x.userNIK == currUserId && x.menuID == 7).Select(x => x.allowUpdate).SingleOrDefault();
+            //}
+            return View();
+        }
+
+       [HttpPost]
+       public ActionResult GetRoomDetails(int ID)
+        {
+            var currUser = ((ClaimsIdentity)User.Identity).GetUserId();
+            var CurrUser = db.V_Users_Active.Where(w => w.NIK == currUser).FirstOrDefault();
+
+            var room = dbm.OTH_MRR_Master_Rooms.Where(w => w.IDRoomCat == ID).ToList();
+            var roomIDs = room.Select(r => r.ID).ToList();
+            var props = dbm.OTH_MRR_Rooms_Properties.Where(w => roomIDs.Contains(w.RoomID)).ToList();
+
+            return Json(new { data = room, prop = props }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult GetSchedule()
+        {
+            var currUser = ((ClaimsIdentity)User.Identity).GetUserId();
+            var CurrUser = db.V_Users_Active.Where(w => w.NIK == currUser).FirstOrDefault();
+
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            Response.ContentType = "text/xml";
+            ViewBag.events = db.Events.ToList();
+            return View();
+        }
+    }
+}
---
+++ NGKBusi/Areas/Other/Models/MRR.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+namespace NGKBusi.Areas.Other.Models
+{
+    public class MRR
+    {
+    }
+
+    public class OTH_MRR_Master_Rooms_Category
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public string Category { get; set; }
+    }
+    
+    public class OTH_MRR_Master_Rooms
+    {
+
+        [Key]
+        public int ID { get; set; }
+        public string RoomTitle { get; set; }
+        public string Image { get; set; }
+        public int IDRoomCat { get; set; }
+        public int? ExtensionNumber { get; set; }
+    }
+
+    public class OTH_MRR_Rooms_Properties
+    {
+        [Key]
+        public int ID { get; set; }
+        public int RoomID { get; set; }
+        public string PropsName { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class OTH_MRR_Bookings
+    {
+        [Key]
+        public int ID { get; set; }
+        public int UserNIK { get; set; }
+        public int RoomsPropsID { get; set; }
+        public string Subject { get; set; }
+        public string Remarks { get; set; }
+        public DateTime Day { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Boolean Status { get; set; }
+        public DateTime Timestamps { get; set; }
+    }
+
+    public class OTH_MRR_Booking_Tools
+    {
+        [Key]
+        public int ID { get; set; }
+        public int BookingID { get; set; }
+        public int ToolID { get; set; }
+    }
+
+    public class OTH_MRR_Master_Additional_Tools
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ToolName { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class JS_OTH_MRR_Master_Rooms
+    {
+
+        public int ID { get; set; }
+        public string RoomTitle { get; set; }
+        public string Image { get; set; }
+        public int IDRoomCat { get; set; }
+        public int? ExtensionNumber { get; set; }
+    }
+    public class Tbl_Rooms_Detail
+    {
+        public int ID { get; set; }
+        public string Location { get; set; }
+        public int ExtensionNumber { get; set; }
+        public int RoomID { get; set; }
+        public int RoomTitle { get; set; }
+        public string PropsName { get; set; }
+        public int Quantity { get; set; }
+    }
+
+
+    public class MRRConnection : DbContext
+    {
+        public DbSet<OTH_MRR_Master_Rooms> OTH_MRR_Master_Rooms { get; set; }
+        public DbSet<OTH_MRR_Master_Rooms_Category> OTH_MRR_Master_Rooms_Category { get; set; }
+        public DbSet<OTH_MRR_Rooms_Properties> OTH_MRR_Rooms_Properties { get; set; }
+        public DbSet<OTH_MRR_Bookings> OTH_MRR_Bookings { get; set; }
+        public DbSet<OTH_MRR_Booking_Tools> OTH_MRR_Booking_Tools { get; set; }
+        public DbSet<OTH_MRR_Master_Additional_Tools> OTH_MRR_Master_Additional_Tools { get; set; }
+        public MRRConnection()
+        {
+            this.Database.Connection.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        }
+    }
+}
